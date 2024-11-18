f0ea8b94d4b32aceeb8c55bb08c51bbe4392bd6d
josuecova2708 <josuecovarrubias8@gmail.com>
2024-11-18 14:42:20 -0400

    errors

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '**', // Permitir cualquier dominio (usar con precaución)
-      },
-    ],
-    eslint: {
-      // Warning: This allows production builds to successfully complete even if
-      // your project has ESLint errors.
-      ignoreDuringBuilds: true,
-    },
-  }
-
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 }
-
-
+ 
 export default nextConfig;
